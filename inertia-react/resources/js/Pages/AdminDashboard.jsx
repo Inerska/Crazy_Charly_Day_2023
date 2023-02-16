1375c1d6143d296cf931c15883c6db0617eec755
Alexis Chân Gridel <alexis.gridel@pm.me>
2023-02-16 14:31:41 +0100

    feat: main dashboard, admin dashbaord and display user count

--- inertia-react/resources/js/Pages/AdminDashboard.jsx
+++ inertia-react/resources/js/Pages/AdminDashboard.jsx
@@ -1,8 +1,19 @@
+import {Button, Card, Container, Text} from "@nextui-org/react";
+
 export default function AdminDashboard(props) {
-    return (
-        <div>
-            <h1>Admin Dashboard</h1>
-            <pre>{JSON.stringify(props, null, 2)}</pre>
-        </div>
-    );
+    return (<>
+            <Container>
+                <h1>Admin Dashboard</h1>
+                <Card>
+                    <Card.Header>
+                        <Text>User Count</Text>
+                    </Card.Header>
+                    <Card.Body>
+                        <Text>{props.userCount}</Text>
+                    </Card.Body>
+                </Card>
+
+                <a href="/dashboard">Return to main dashboard</a>
+            </Container>
+        </>);
 }
