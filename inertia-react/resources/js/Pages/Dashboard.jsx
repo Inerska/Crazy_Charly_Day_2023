cb242ef1e7bd9505e6a7c7ef30e96a644ee1492c
Alexis Chân Gridel <alexis.gridel@pm.me>
2023-02-16 21:10:43 +0100

    feat: center all main document

--- inertia-react/resources/js/Pages/Dashboard.jsx
+++ inertia-react/resources/js/Pages/Dashboard.jsx
@@ -13,57 +13,63 @@ export default function Dashboard(props) {
         header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">All products</h2>}
     >
         <Head title="Court Circuit"/>
-
-        <div className="py-12">
-            <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <Grid.Container gap={2} justify="center">
-                    {Object.entries(props.produits).map(([key, value]) => {
-                        return (<Grid key={key} xs={12} sm={6} md={4} lg={3} xl={2}>
-                            <Card isPressable isHoverable>
-                                <a href={`/product/${value.id}`}>
-                                    <Card.Body css={{p: 0}}>
-                                        <Card.Image
-                                            src={`/products/${value.id}.jpg`}
-                                            width={140}
-                                            height={140}
-                                            objectFit="cover"
-                                            alt={value.description}
-                                        />
-                                    </Card.Body>
-                                    <Card.Footer css={{justifyItems: "flex-start"}}>
-                                        <Row wrap="wrap" justify="space-between" align="center">
-                                            <Text b>{value.nom}</Text>
-                                            <Text css={{color: "$accents7", fontWeight: "$semibold", fontSize: "$sm"}}>
-                                                {value.prix} €/{value.poids === 0 ? "kg" : "unité"}
-                                            </Text>
-                                            <Button auto size="small" type="success">
-                                                <a href={`/add-to-panier/${value.id}`}>
-                                                    Add to cart
-                                                </a>
-                                            </Button>
-                                        </Row>
-                                    </Card.Footer>
-                                </a>
-                            </Card>
-                        </Grid>)
-                    })}
-                </Grid.Container>
+        <div className="flex flex-col items-center">
+            <div className="py-12">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <Grid.Container gap={2} justify="center">
+                        {Object.entries(props.produits).map(([key, value]) => {
+                            return (<Grid key={key} xs={12} sm={6} md={4} lg={3} xl={2}>
+                                <Card isPressable isHoverable>
+                                    <a href={`/product/${value.id}`}>
+                                        <Card.Body css={{p: 0}}>
+                                            <Card.Image
+                                                src={`/products/${value.id}.jpg`}
+                                                width={140}
+                                                height={140}
+                                                objectFit="cover"
+                                                alt={value.description}
+                                            />
+                                        </Card.Body>
+                                        <Card.Footer css={{justifyItems: "flex-start"}}>
+                                            <Row wrap="wrap" justify="space-between" align="center">
+                                                <Text b>{value.nom}</Text>
+                                                <Text css={{
+                                                    color: "$accents7",
+                                                    fontWeight: "$semibold",
+                                                    fontSize: "$sm"
+                                                }}>
+                                                    {value.prix} €/{value.poids === 0 ? "kg" : "unité"}
+                                                </Text>
+                                                <Button auto size="small" type="success">
+                                                    <a href={`/add-to-panier/${value.id}`}>
+                                                        Add to cart
+                                                    </a>
+                                                </Button>
+                                            </Row>
+                                        </Card.Footer>
+                                    </a>
+                                </Card>
+                            </Grid>)
+                        })}
+                    </Grid.Container>
+                </div>
             </div>
+
+            <Pagination total={props.buttons} page={page} onChange={(page) => {
+                setPage(page)
+
+                router.visit(`/dashboard/${page}`,
+
+                    {
+                        preserveScroll: true,
+                        only: ['produits'],
+                        preserveState: true,
+                    })
+
+
+            }}/>
         </div>
 
-        <Pagination total={props.buttons} page={page} onChange={(page) => {
-            setPage(page)
-
-            router.visit(`/dashboard/${page}`,
-
-                {
-                    preserveScroll: true,
-                    only: ['produits'],
-                    preserveState: true,
-                })
-
-
-        }}/>
 
     </AuthenticatedLayout>);
 }
