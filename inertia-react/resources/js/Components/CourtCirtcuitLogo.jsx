643032ff1967776a703fd51ec2647d2683d24979
Alexis Chân Gridel <alexis.gridel@pm.me>
2023-02-16 17:51:09 +0100

    feat: enhance product page design

--- inertia-react/resources/js/Components/CourtCirtcuitLogo.jsx
+++ inertia-react/resources/js/Components/CourtCirtcuitLogo.jsx
@@ -1,5 +1,5 @@
 export default function CourtCircuitLogo() {
     return <>
-        <img src="court-circuit-logo.png" alt="Court Circuit Logo" height="50" width="50" />
+        <img src="/court-circuit-logo.png" alt="Court Circuit Logo" height="50" width="50" />
     </>
 }
--- inertia-react/resources/js/Pages/ProductPage.jsx
+++ inertia-react/resources/js/Pages/ProductPage.jsx
@@ -1,30 +1,50 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
-import {Card, Image} from "@nextui-org/react";
+import {Container, Image} from "@nextui-org/react";
 
 export default function ProductPage(props) {
-    return (
-        <AuthenticatedLayout
-            auth={props.auth}
-            errors={props.errors}
-            header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">All products</h2>}>
+    return (<AuthenticatedLayout
+        auth={props.auth}
+        errors={props.errors}
+        header={<h2 className="font-semibold text-xl text-gray-800 leading-tight">All products</h2>}>
 
-            <Image
-                src={`/products/${props.product.id}.jpg`}
-                width={140}
-                height={140}
-                objectFit="cover"
-                alt={props.product.description}
-            />
+        <Container className="mt-6 ">
+            <div className="flex flex-col md:flex-row justify-between">
+                <div className="mr-6">
+                    <Image
+                        src={`/products/${props.product.id}.jpg`}
+                        width="400"
+                        height="400"
+                        objectFit="cover"
+                        alt={props.product.description}
+                    />
+                </div>
+                <div>
+                    <h1 class="text-5xl font-bold mb-6">{props.product.nom}</h1>
+                    <p>{props.product.description}</p>
+                    <hr className="mt-2 mb-2"/>
+                    <p class="text-3xl font-semibold">{props.product.prix} €/{props.product.poids === 0 ? "kg" : "unité"}</p>
+                    <p>Payement sécurisé avec Stripe, Paypal ou Apple Pay</p>
+                    <hr className="mt-2 mb-2"/>
 
-            <h1 class="text-2xl">{props.product.nom}</h1>
-            <p class="font-semibold">{props.product.description}</p>
-            <p class="text-gray-800">{props.product.prix} €/{props.product.poids === 0 ? "kg" : "unité"}</p>
+                    <a href={`/add-to-panier/${props.product.id}`}
+                       class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg mt-6 font-2xl">
+                        Add to cart
+                    </a>
 
+                    <table class="table-auto mt-6">
+                        <tbody>
+                        <tr>
+                            <td class="border px-4 py-2">Cheapest delivery fee</td>
+                        </tr>
+                        <tr>
+                            <td class="border px-4 py-2">Symmetrically designed</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                </div>
 
-            <a href={`/add-to-panier/${props.product.id}`} class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                Add to cart
-            </a>
+            </div>
 
-        </AuthenticatedLayout>
-    )
+        </Container>
+    </AuthenticatedLayout>)
 }
