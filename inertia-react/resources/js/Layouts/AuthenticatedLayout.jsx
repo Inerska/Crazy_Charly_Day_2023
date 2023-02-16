714eee11d8a945f0b1620262000e95c0a5dfa33e
Alexis Chân Gridel <alexis.gridel@pm.me>
2023-02-16 14:03:42 +0100

    feat: old logo to cc logo

--- inertia-react/resources/js/Layouts/AuthenticatedLayout.jsx
+++ inertia-react/resources/js/Layouts/AuthenticatedLayout.jsx
@@ -5,6 +5,7 @@ import NavLink from '@/Components/NavLink';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
 import { Link } from '@inertiajs/react';
 import {Avatar, User} from "@nextui-org/react";
+import CourtCircuitLogo from "@/Components/CourtCirtcuitLogo";
 
 export default function Authenticated({ auth, header, children }) {
     const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
@@ -17,7 +18,7 @@ export default function Authenticated({ auth, header, children }) {
                         <div className="flex">
                             <div className="shrink-0 flex items-center">
                                 <Link href="/">
-                                    <ApplicationLogo className="block h-9 w-auto fill-current text-gray-800" />
+                                    <CourtCircuitLogo className="block h-9 w-auto fill-current text-gray-800" />
                                 </Link>
                             </div>
 
