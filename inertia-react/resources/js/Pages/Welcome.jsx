b041f59c9a393de7ead69bc814487be36606f70f
Alexis Chân Gridel <alexis.gridel@pm.me>
2023-02-16 18:52:59 +0100

    feat: remove useless buttons

--- inertia-react/resources/js/Pages/Welcome.jsx
+++ inertia-react/resources/js/Pages/Welcome.jsx
@@ -22,21 +22,7 @@ export default function Welcome(props) {
                         Court Circuit
                     </Text>
                 </Navbar.Brand>
-                {
-                    !props.auth.user ? (
-                            <Navbar.Content hideIn="xs">
-                                <Navbar.Link href="#">Features</Navbar.Link>
-                                <Navbar.Link isActive href="#">Customers</Navbar.Link>
-                                <Navbar.Link href="#">Pricing</Navbar.Link>
-                                <Navbar.Link href="#">Company</Navbar.Link>
-                            </Navbar.Content>
-                    ) :
-                    (
-                        <Navbar.Content hideIn="xs">
-                            <ProductsSearchBar />
-                        </Navbar.Content>
-                    )
-                }
+
 
                 <Navbar.Content>
                     {props.auth.user ? (
