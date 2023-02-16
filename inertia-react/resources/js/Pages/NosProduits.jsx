db5a0e2b96829eeb94ff7297ae5f32da78509164
Alexis Chân Gridel <alexis.gridel@pm.me>
2023-02-16 22:51:58 +0100

    fix(index): lts version map

--- inertia-react/resources/js/Pages/NosProduits.jsx
+++ inertia-react/resources/js/Pages/NosProduits.jsx
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from "react";
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import React, {useEffect, useState} from "react";
+import {MapContainer, Marker, Popup, TileLayer} from "react-leaflet";
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
+const AnyReactComponent = ({text}) => <div>{text}</div>;
 
 export default function NosProduits(props) {
     const [products, setProducts] = useState([]);
@@ -23,16 +23,19 @@ export default function NosProduits(props) {
     const position = [48.725288, 2.572804];
 
     return (
-        <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            {geoProps.map((geoProp, index) => (
-                <Marker key={index} position={geoProp.position}>
-                    <Popup>{geoProp.nom}</Popup>
-                </Marker>
-            ))}
-        </MapContainer>
+        <div style={{height: "100vh", width: "100%"}}>
+            <MapContainer center={position} zoom={13} scrollWheelZoom={false}
+                            style={{height: "100vh", width: "100%"}}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                {geoProps.map((geoProp, index) => (
+                    <Marker key={index} position={geoProp.position}>
+                        <Popup>{geoProp.nom}</Popup>
+                    </Marker>
+                ))}
+            </MapContainer>
+        </div>
     );
 }
